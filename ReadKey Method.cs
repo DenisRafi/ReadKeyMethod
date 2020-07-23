35a08eb8f763717d26fe21597172caacbfb40524
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-07-23 01:07:12 +0100

    Add files via upload

---
+++ ReadKey Method.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class ByDenisRafi
+{
+    public static void Main()
+    {
+        Console.Title = "ReadKey Method";
+        DateTime dat = DateTime.Now;
+        Console.WriteLine("The time: {0:d} at {0:t}", dat);
+        TimeZoneInfo tz = TimeZoneInfo.Local;
+        Console.WriteLine("The time zone: {0}\n",
+                          tz.IsDaylightSavingTime(dat) ?
+                             tz.DaylightName : tz.StandardName);
+        Console.Write("Press <Enter> to exit... ");
+        while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+    }
+}
